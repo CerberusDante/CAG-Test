f26d518819f0dae58b199a670cb1293df5dab678
CerberusDante <1500575702@qq.com>
2019-05-08 17:22:15 +0800

    添加项目文件。

---
+++ 2DGraphics/2DGraphics.h
@@ -0,0 +1,40 @@
+﻿
+// 2DGraphics.h: 2DGraphics 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMy2DGraphicsApp:
+// 有关此类的实现，请参阅 2DGraphics.cpp
+//
+
+class CMy2DGraphicsApp : public CWinAppEx
+{
+public:
+	CMy2DGraphicsApp() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy2DGraphicsApp theApp;
---
+++ 2DGraphics/2DGraphicsView.cpp
@@ -0,0 +1,127 @@
+﻿
+// 2DGraphicsView.cpp: CMy2DGraphicsView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "2DGraphics.h"
+#endif
+
+#include "2DGraphicsDoc.h"
+#include "2DGraphicsView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMy2DGraphicsView
+
+IMPLEMENT_DYNCREATE(CMy2DGraphicsView, CView)
+
+BEGIN_MESSAGE_MAP(CMy2DGraphicsView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMy2DGraphicsView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMy2DGraphicsView 构造/析构
+
+CMy2DGraphicsView::CMy2DGraphicsView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMy2DGraphicsView::~CMy2DGraphicsView()
+{
+}
+
+BOOL CMy2DGraphicsView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMy2DGraphicsView 绘图
+
+void CMy2DGraphicsView::OnDraw(CDC* /*pDC*/)
+{
+	CMy2DGraphicsDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMy2DGraphicsView 打印
+
+
+void CMy2DGraphicsView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMy2DGraphicsView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMy2DGraphicsView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMy2DGraphicsView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CMy2DGraphicsView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMy2DGraphicsView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMy2DGraphicsView 诊断
+
+#ifdef _DEBUG
+void CMy2DGraphicsView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMy2DGraphicsView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMy2DGraphicsDoc* CMy2DGraphicsView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMy2DGraphicsDoc)));
+	return (CMy2DGraphicsDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMy2DGraphicsView 消息处理程序
