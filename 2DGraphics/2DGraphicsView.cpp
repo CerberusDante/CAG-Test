44d28c1d98d48134c1000302f6a644ac72ad5437
CerberusDante <1500575702@qq.com>
2019-05-23 15:20:43 +0800

    添加圆角矩形等绘制函数

--- 2DGraphics/2DGraphicsView.cpp
+++ 2DGraphics/2DGraphicsView.cpp
@@ -60,6 +60,168 @@ void CMy2DGraphicsView::OnDraw(CDC* /*pDC*/)
 		return;
 
 	// TODO: 在此处为本机数据添加绘制代码
+
+	//修改坐标系映射
+
+	CDC *pDC = GetDC();
+	CRect rect;											//定义矩形
+	GetClientRect(&rect);								//获得客户去矩形的大小
+	pDC->SetMapMode(MM_ANISOTROPIC);					//自定义坐标系映射模式
+	pDC->SetWindowExt(rect.Width(), rect.Height());		//设置窗口比例
+	pDC->SetViewportExt(rect.Width(), -rect.Height());	//设置视区比例，且x轴水平向右，y轴垂直向上
+	pDC->SetViewportOrg(rect.Width() / 2, rect.Height() / 2);
+														//设置客户区中心为坐标原点
+	rect.OffsetRect(-rect.Width() / 2, -rect.Height() / 2);
+														//矩形与客户区重合
+	srand((unsigned)time(NULL));
+
+	/*for (int x = 200; x <= 233; x++) {
+		for (int y = 300; y <= 333; y++)
+		{
+			CPoint P(x, y);
+			pDC->SetPixelV(x, y, RGB(255, 0, 0));
+		}
+	}
+
+	int a, b;
+	COLORREF crColor;
+	for (a = -50; a < 50; a++)
+	{
+		for (b = -150; b < -50; b++)
+		{
+			pDC->SetPixelV(b, a, RGB(rand() % 255, rand() % 255, rand() % 255));
+		}
+	}
+
+	for (a = -50; a < 50; a++)
+	{
+		for (b = -150; b < -50; b++)
+		{
+			crColor = pDC->GetPixel(b, a);
+			pDC->SetPixelV(-b, a, crColor);
+		}
+	}*/
+	
+	{
+		pDC->SetPixelV(200, 300, RGB(255, 0, 0));
+		// 在坐标200，300 画点，RGB为红色
+	}
+
+	{
+		pDC->MoveTo(100, 100);
+		pDC->LineTo(200, 300);
+		// 从(100, 100)到(200,300) 画直线
+	}
+
+	{
+		CPen RedPen, *pOldPen;
+		RedPen.CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
+		pOldPen = pDC->SelectObject(&RedPen);
+		/*
+			在这里插入画图代码段
+		*/
+
+		pDC->SelectObject(pOldPen);
+		RedPen.DeleteObject();
+		//创建红色画笔，宽度为2
+	}
+
+	{
+		CPen BluePen, *qOldPen;
+		BluePen.CreatePen(PS_SOLID, 3, RGB(0, 0, 255));
+		qOldPen = pDC->SelectObject(&BluePen);
+
+		pDC->MoveTo(50, 20);
+		pDC->LineTo(200, 100);
+
+		pDC->MoveTo(-50, 20);
+		pDC->LineTo(-200, 100);
+
+		pDC->SelectObject(qOldPen);
+		BluePen.DeleteObject();
+
+
+		pDC->Rectangle(100, 100, 400, 300);
+		pDC->Ellipse(100, 100, 400, 300);
+	}
+
+	{
+		//画刷函数
+		CBrush RedSolidBrush, RedHatchBrush, *pOldBrush;
+		//SolidBrush —— 实心画刷
+		//HatchBrush —— 空心画刷
+
+		//实心画刷
+		RedSolidBrush.CreateSolidBrush(RGB(255, 0, 0));
+		pOldBrush = pDC->SelectObject(&RedSolidBrush);
+
+		pDC->Ellipse(45, 15, 55, 25);
+		pDC->Ellipse(195, 95, 205, 105);
+
+		pDC->Ellipse(-45, 15, -55, 25);
+		pDC->Ellipse(-195, 95, -205, 105);
+	
+		pDC->SelectObject(pOldBrush);
+		RedSolidBrush.DeleteObject();
+
+		//空心画刷
+		RedHatchBrush.CreateHatchBrush(HS_BDIAGONAL, RGB(255, 0, 0));
+		pOldBrush = pDC->SelectObject(&RedHatchBrush);
+		/*	
+			在此处添加代码段
+		*/
+		pDC->SelectObject(pOldBrush);
+		RedHatchBrush.DeleteObject();
+	}		
+	
+	//绘制圆角矩形并填充
+	{
+		/*rect.DeflateRect(100, 100);//将矩形两个方向都缩小100个像素
+		CPen NewPen, *pOldPen;
+		NewPen.CreatePen(PS_SOLID, 1, RGB(0, 0, 255));
+		pOldPen = pDC->SelectObject(&NewPen);
+		pDC->Rectangle(rect);
+		CBrush NewBrush, *pOldBrush;
+		NewBrush.CreateSolidBrush(RGB(255, 0, 0));
+		pOldBrush = pDC->SelectObject(&NewBrush);
+		pDC->RoundRect(rect, CPoint(200, 200));//绘制圆角矩形
+		pDC->SelectObject(pOldPen);
+		pDC->SelectObject(pOldBrush);*/
+	}
+	//填充矩形
+	{
+		CRect rect1(CPoint(-250, 50), CPoint(250, 150));
+		CRect rect2(CPoint(-250, -150), CPoint(250, -50));
+		CBrush Brush(HS_BDIAGONAL, RGB(255, 0, 0));//定义红色阴影画刷
+		pDC->FillRect(&rect1, &Brush);//画刷填充
+		pDC->FillSolidRect(&rect2, RGB(255, 0, 0));//红色填充
+	}
+	//绘制椭圆
+	{
+		/*rect.DeflateRect(100, 100);
+		CBrush *pOldBrush;
+		pOldBrush = (CBrush*)pDC->SelectStockObject(NULL_BRUSH);//选择透明画刷
+		int r = rect.Height() / 2;								//定义圆的半径
+		CRect rect1(CPoint(-r, -r), CPoint(r, r));				//定义圆的外接矩形
+		pDC->Ellipse(rect1);									//绘制圆
+		pDC->Ellipse(rect);										//绘制椭圆
+		pDC->Rectangle(rect);									//绘制外接矩形
+		pDC->SelectObject(pOldBrush);*/
+	} 
+	//绘制圆弧
+	{
+		CPoint Twelve(0, rect.Height() / 2), Three(rect.Width() / 2, 0);
+		CPen penBlack, penBlue, *pOldPen;
+		penBlue.CreatePen(PS_SOLID, 1, RGB(0, 0, 255));
+		penBlack.CreatePen(PS_DASHDOT, 1, RGB(0, 0, 0));
+		pDC->SetArcDirection(AD_CLOCKWISE);
+		pOldPen = pDC->SelectObject(&penBlue);
+		pDC->Arc(rect, Twelve, Three);
+		pDC->SetArcDirection(AD_COUNTERCLOCKWISE);
+		pOldPen = pDC->SelectObject(&penBlack);
+		pDC->Arc(rect, Twelve, Three);
+		pDC->SelectObject(pOldPen);
+	}
 }
 
 
